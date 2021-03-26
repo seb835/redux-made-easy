941f4e59186237ab080de63ac6dfcdea12bea89c
seb835 <seb@propeller-studios.co.uk>
2021-03-26 16:12:59 +0000

    We can also use Reacts own state management in conjunction with Redux

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import "./App.css";
 import { useSelector } from "react-redux";
 
 export default function App() {
+  // The total count stored in the redux store
   const count = useSelector((state) => state.counter.count);
+  const voters = [
+    "Seb Pedley",
+    "Bob Smith",
+    "Merango Shelateran",
+    "Suungo Parlance",
+  ];
 
   return (
     <div className="App">
       <h1>Redux made easy</h1>
-      <h2>The count: {count}</h2>
-      <Counter />
+      <h2>Total Votes: {count}</h2>
+      {voters.map((voter) => (
+        <Counter name={voter} />
+      ))}
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { increment, decrement } from "./redux/ducks/counter";
 
-const Counter = () => {
+const Counter = (props) => {
+  const { name } = props;
   const dispatch = useDispatch();
+  const [votes, setVotes] = useState(0);
 
   const handleIncrement = () => {
     dispatch(increment());
+    setVotes(votes + 1);
   };
   const handleDecrement = () => {
     dispatch(decrement());
+    setVotes(votes - 1);
   };
 
   return (
     <div style={{ backgroundColor: "grey", margin: "10px" }}>
+      <h2>{name}</h2>
+      <h3>{`Votes: ${votes}`}</h3>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <button onClick={handleIncrement}>Increment</button>
         <button onClick={handleDecrement}>Decrement</button>
